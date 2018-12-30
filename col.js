cab77dedbfdc3e516532eeec027f4bc5e02c9b3b
richreader <46259366+richreader@users.noreply.github.com>
2018-12-30 18:28:16 +0800

    Update col.js

--- col.js
+++ col.js
@@ -1,4 +1,4 @@
-﻿var widgetHtml = '<iframe src="https://www.prepostseo.com/widget/plagiarismchecker" style="border:0;width:100%;min-height:400px;height:100%;overflow: hidden;"></iframe>';
+var widgetHtml = '<iframe src="https://www.prepostseo.com/widget/plagiarismchecker" style="border:0;width:100%;min-height:400px auto;height:100% auto;overflow: hidden;"></iframe>';
 var widgetTag = document.getElementById("ppsLink");
 if (widgetTag === null){ 
 	widgetHtml += '<div style="text-align: center; font-size:12px; color:#333;margin-top:-40px;"><p><a href="https://writeforbenefits.blogspot.com" target="_blank" style="color:#000;" id="ppsLink">Plagiarism Checker</a> provided by <a href="https://www.prepostseo.com/" rel="nofollow noreferrer noopener noindex" target="_blank"  style="color:#000;">prepostseo.com</a></p></div>';
@@ -12,7 +12,7 @@ if (widgetTag === null){
 
 var isNew = document.getElementById("ppsWidgetCode");
 if(isNew === null){
-	document.write('<iframe src="https://www.prepostseo.com/widget/plagiarismchecker" style="border:0;width:100%;min-height:400px;height:100%;overflow: hidden;"></iframe>');
+	document.write('<iframe src="https://www.prepostseo.com/widget/plagiarismchecker" style="border:0;width:100%;min-height:400px auto;height:100% auto;overflow: hidden;"></iframe>');
 } else {
 	document.getElementById("ppsWidgetCode").innerHTML = widgetHtml;
 }
