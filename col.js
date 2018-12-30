7fcfe186e2801b7eca200d574bff55f940051a27
richreader <46259366+richreader@users.noreply.github.com>
2018-12-30 18:50:17 +0800

    Update col.js

--- col.js
+++ col.js
@@ -1,12 +1,12 @@
 var widgetHtml = '<iframe src="https://www.prepostseo.com/widget/plagiarismchecker" style="border:0;width:100%;min-height:400px auto;height:100% auto;overflow: hidden;"></iframe>';
 var widgetTag = document.getElementById("ppsLink");
 if (widgetTag === null){ 
-	widgetHtml += '<div style="text-align: center; font-size:12px; color:#e2e2e2;margin-top:-40px;"><p><a href="https://writeforbenefits.blogspot.com" target="_blank" style="color:#e2e2e2;" id="ppsLink">Plagiarism Checker</a> provided by <a href="https://www.prepostseo.com/" rel="nofollow noreferrer noopener noindex" target="_blank"  style="color:#e2e2e2;">PrePostSEO</a></p></div>';
+	widgetHtml += '<div style="text-align: center; font-size:12px; color:#e2e2e2;margin-top:-40px;"><p><a href="https://writeforbenefits.blogspot.com/p/plagiarism-checker.html" target="parent" style="color:#e2e2e2;" id="ppsLink">Plagiarism Checker</a> provided by <a href="https://www.prepostseo.com/" rel="nofollow noreferrer noopener noindex" target="_blank"  style="color:#e2e2e2;">PrePostSEO</a></p></div>';
 	
 } else {
 	var linkUrl = widgetTag.getAttribute("href");	
-	if(linkUrl !== 'https://writeforbenefits.blogspot.com'){
-		widgetHtml += '<div style="text-align: center; font-size:12px; color:#333;margin-top:-40px;"><p><a href="https://writeforbenefits.blogspot.com" target="_blank" style="color:#e2e2e2;" id="ppsLink">Plagiarism Checker</a> provided by <a href="https://www.prepostseo.com/" rel="nofollow noreferrer noopener noindex" target="_blank"  style="color:#e2e2e2;">PrePostSEO</a></p></div>';
+	if(linkUrl !== 'https://writeforbenefits.blogspot.com/p/plagiarism-checker.html'){
+		widgetHtml += '<div style="text-align: center; font-size:12px; color:#333;margin-top:-40px;"><p><a href="https://writeforbenefits.blogspot.com/p/plagiarism-checker.html" target="parent" style="color:#e2e2e2;" id="ppsLink">Plagiarism Checker</a> provided by <a href="https://www.prepostseo.com/" rel="nofollow noreferrer noopener noindex" target="_blank"  style="color:#e2e2e2;">PrePostSEO</a></p></div>';
 	}
 }
 
